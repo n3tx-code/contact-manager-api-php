0520fc5474cd56bd0ed51ff5a8af0cc81a8058a4
Nils <nils@vaede.com>
2019-12-06 00:13:40 +0100

    typo in variable

--- contact/delete/index.php
+++ contact/delete/index.php
@@ -6,8 +6,8 @@ if(isset($_POST['token']) AND isset($_POST['ID']) AND isset($_POST['ID_owner']))
 {
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/_includes/models/Contact.php");
     $contact = new Contact();
-    $update_contact_status = $contact->delete($_POST['token'], $_POST['ID'], $_POST['ID_owner']);
-    echo $update_contact_status;
+    $delete_contact_status = $contact->delete($_POST['token'], $_POST['ID'], $_POST['ID_owner']);
+    echo $delete_contact_status;
 }
 else
 {
